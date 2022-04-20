e36a8b012987d8f6b41d866a27ee94a89409cab5
sssbatcha <99872239+sssbatcha@users.noreply.github.com>
2022-04-20 03:41:52 +0000

    Wed, Apr 20, 2022, 3:41 AM +00:00

---
+++ coke/coke.py
@@ -0,0 +1,17 @@
+amount_due = 50
+while amount_due > 0:
+    insert_coin = int(input("Insert coin: "))
+
+    if insert_coin in [25,10,5]:
+        amount_due-= insert_coin
+        if amount_due>0:
+            print(f"Amount Due: {amount_due}")
+        elif amount_due < 0:
+            change_owed = abs(amount_due)
+        else:
+            change_owed = 0
+    else:
+        print(f"Amount Due: {amount_due}")
+        continue
+
+print(f"Change Owed: {change_owed}")
