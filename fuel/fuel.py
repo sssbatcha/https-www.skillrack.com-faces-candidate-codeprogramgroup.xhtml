42225b6543c1ca643a44eff389070f1c66fb25b9
sssbatcha <99872239+sssbatcha@users.noreply.github.com>
2022-04-27 10:13:31 +0000

    Wed, Apr 27, 2022, 10:13 AM +00:00

--- fuel/fuel.py
+++ fuel/fuel.py
@@ -9,27 +9,27 @@ def func():
         c=(b/d)
         if(c==0.0):
             print('E')
-            func()
+
         elif(0.0<c<=0.25):
             print('25%')
-            func()
+
         elif(0.26<=c<=0.50):
            print('50%')
-           func()
+
         elif(0.50<c<=0.75):
             print('75%')
-            func()
+
         elif(0.75<c<=1.0):
             print('F')
-            func()
+
         elif (c>1.0):
             print('not valid')
-            func()
+
     except ZeroDivisionError:
         print("cannot divide by zero")
-        func()
+
     except ValueError:
         print("value error")
-        func()
+
 
 func()
