9dad515845befe9710990f0ec5277597cebe89bb
sssbatcha <99872239+sssbatcha@users.noreply.github.com>
2022-04-27 08:25:07 +0000

    Wed, Apr 27, 2022, 8:25 AM +00:00

--- fuel/fuel.py
+++ fuel/fuel.py
@@ -1,20 +1,20 @@
 
 try:
 
-    z=int(input())/int(input())
-    if(0.0<z<0.24):
+    z=str(input())
+    a=int(z[0])
+    b=int(z[2])
+    c=a/b
+
+    if(0.0<c<=0.25):
         print('E')
-    if(0.25<z<=0.50):
+    if(0.26<=c<=0.50):
         print('50%')
-    if(0.51<z<=0.75):
+    if(0.50<c<=0.75):
         print('75%')
-    if(0.76<z<1.0):
+    if(0.75<c<=1.0):
         print('E')
 except ZeroDivisionError:
     print("cannot divide by zero")
 except ValueError:
     print("value error")
-
-
-
-
