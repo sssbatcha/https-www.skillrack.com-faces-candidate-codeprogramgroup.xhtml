820d19fc383ee904385b18ac20f29616c4a54e37
sssbatcha <99872239+sssbatcha@users.noreply.github.com>
2022-04-28 10:13:41 +0000

    Thu, Apr 28, 2022, 10:13 AM +00:00

--- fuel/fuel.py
+++ fuel/fuel.py
@@ -1,35 +1,18 @@
-from fractions import Fraction as frac
-
-def func():
-
+while True:
+    fuel = input("Fraction: ")
     try:
-        a=input()
-        b=(frac(a).numerator)
-        d=(frac(a).denominator)
-        c=(b/d)
-        if(c==0.0):
-            print('E')
-
-        elif(0.0<c<=0.25):
-            print('25%')
-
-        elif(0.26<=c<=0.50):
-           print('50%')
-
-        elif(0.50<c<=0.75):
-            print('75%')
-
-        elif(0.75<c<=1.0):
-            print('F')
-
-        elif (c>1.0):
-            print('not valid')
-
-    except ZeroDivisionError:
-        print("cannot divide by zero")
-
-    except ValueError:
-        print("value error")
-
-
-func()
+        numerator, denominator = fuel.split("/")
+        new_numerator = int(numerator)
+        new_denominator = int(denominator)
+        p = new_numerator / new_denominator
+        if p <= 1:
+            break
+    except (ValueError, ZeroDivisionError):
+        pass
+new_p = int(p * 100)
+if new_p <= 1:
+    print("E")
+elif new_p >= 99:
+    print("F")
+else:
+    print(f"{new_p}%")
