47698cb197680f0c733eb2025f14fc66f98dc43b
sssbatcha <99872239+sssbatcha@users.noreply.github.com>
2022-06-16 04:44:32 +0000

    Thu, Jun 16, 2022, 4:44 AM +00:00

--- shirtificate/shirtificate.py
+++ shirtificate/shirtificate.py
@@ -1,27 +1,8 @@
 from fpdf import FPDF
-
-class PDF():
-    def _init_(self, name):
-        self._pdf = FPDF()
-        self._pdf.add_page()
-        self._pdf.set_font("helvetica", size=50)
-        # Create the Heading
-        self._pdf.cell(w=0, h=60, txt="CS50 Shirtificate",
-                       new_x="LMARGIN", new_y="NEXT", align="C")
-        # Create the Image
-        self._pdf.image("shirtificate.png", w=self._pdf.epw,
-                        alt_text="CS50 Shirtificate")
-        # Create the Text in the Image
-        self._pdf.set_font_size(30)
-        self._pdf.set_text_color(255, 255, 255)
-        self._pdf.text(x=47.5, y=140, txt=f"{name} took CS50")
-    # Save the changes
-    def save(self, name):
-        self._pdf.output(name)
-
-#Get user's name
-name = input("Name: ")
-# Create pdf
-pdf = PDF(name)
-# Create output file
-pdf.save("shirtificate.pdf")
+fp =  open('downloads/boot.txt', 'r')
+pdf = FPDF()
+pdf.add_page()
+pdf.set_font('Arial', '', 11)
+line = fp.read()
+pdf.multi_cell(200, 5, line, 0, 1)
+pdf.output('test.pdf', 'F')
