1fdfdd6ed8542d9411b12aefbd427c6f60bf8883
sssbatcha <99872239+sssbatcha@users.noreply.github.com>
2022-06-16 05:14:24 +0000

    Thu, Jun 16, 2022, 5:14 AM +00:00

--- shirtificate/shirtificate.py
+++ shirtificate/shirtificate.py
@@ -1,13 +1,4 @@
 from fpdf import FPDF
-fp =  open('downloads/boot.txt', 'r')
-pdf = FPDF()
-pdf.add_page()
-pdf.set_font('Arial', '', 11)
-line = fp.read()
-pdf.multi_cell(200, 5, line, 0, 1)
-pdf.output('test.pdf', 'F')
-
-from fpdf import FPDF
 
 class PDF():
     def _init_(self, name):
